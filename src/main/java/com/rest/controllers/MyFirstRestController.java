29b8970b94992021bf8c6997b7fc5881c1358c09
Hnas <hamza.nassour13@gmail.com>
2022-10-19 19:37:31 +0100

    Exception handling with @ExceptionHndler @ResponseStatus Responsentity

--- src/main/java/com/rest/controllers/MyFirstRestController.java
+++ src/main/java/com/rest/controllers/MyFirstRestController.java
@@ -1,11 +1,17 @@
 package com.rest.controllers;
 
 import com.rest.dao.StudentDao;
+import com.rest.entities.GetStudentErrorResponse;
 import com.rest.entities.Student;
+import com.rest.exceptions.SomeThingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.rest.exceptions.StudentNotFoundException;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -15,22 +21,29 @@ public class MyFirstRestController {
     StudentDao studentDao;
 
         @GetMapping(value="/students" , produces = {MediaType.APPLICATION_XML_VALUE , MediaType.APPLICATION_JSON_VALUE})
-        public List<Student> getStudents(){
-            return studentDao.getAllStudents();
+        public ResponseEntity<List<Student>> getStudents() throws SomeThingException {
+            if(1==1){
+                throw new SomeThingException("just for testing");
+            }
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(studentDao.getAllStudents());
         }
 
         @GetMapping(value="/student/{id}" ,params ={"meth1"} ,produces = {MediaType.APPLICATION_XML_VALUE , MediaType.APPLICATION_JSON_VALUE})
-        public Student getStudentById(@PathVariable int id){
+        public Student getStudentById(@PathVariable int id) throws StudentNotFoundException {
+            if(studentDao.getAllStudents().size() < id){
+                throw new  StudentNotFoundException("we can not find this student try with a < id ");
+            }
             return studentDao.getStudentDataById(id);
         }
 
-
-    @GetMapping(value="/student/{name}" ,params ={"meth2"} , produces = {MediaType.APPLICATION_XML_VALUE , MediaType.APPLICATION_JSON_VALUE})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @GetMapping(value="/student/{name}" , produces = {MediaType.APPLICATION_XML_VALUE , MediaType.APPLICATION_JSON_VALUE})
     public Student getStudentByName(@PathVariable String name){
         System.out.println(name);
             return studentDao.getStudentDataByName(name);
 
     }
+    // add a student with @RequestParam
     //@PostMapping("/student")
    // public List<Student> saveStudent(@RequestParam int id , @RequestParam String name , @RequestParam String cne , @RequestParam int level , @RequestParam String country){
 
@@ -39,11 +52,31 @@ public class MyFirstRestController {
       //      return studentDao.getAllStudents();
    // }
 
-    @PostMapping("/student")
-    public List<Student> saveStudent(@RequestBody Student student){
+    @PostMapping(value ="/student" ,produces = {MediaType.APPLICATION_XML_VALUE , MediaType.APPLICATION_JSON_VALUE} , consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Student> saveStudent(@RequestBody Student student ){
         System.out.println(student);
            studentDao.saveStudent(student);
            return studentDao.getAllStudents();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<GetStudentErrorResponse> handleStudentNotFoundException(StudentNotFoundException studentNotFoundException){
+            System.out.println("exception handler");
+            GetStudentErrorResponse response = new GetStudentErrorResponse();
+            response.setDate(new Date());
+            response.setErrorClass(studentNotFoundException.getClass().toString());
+            response.setErrorMessage(studentNotFoundException.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ExceptionHandler
+    public GetStudentErrorResponse handleSomeThingException(SomeThingException someThingException){
+        System.out.println("exception handler for someThingException");
+        GetStudentErrorResponse response = new GetStudentErrorResponse();
+        response.setDate(new Date());
+        response.setErrorClass(someThingException.getClass().toString());
+        response.setErrorMessage(someThingException.getMessage());
+        return response;
     }
 
 
---
+++ src/main/java/com/rest/entities/GetStudentErrorResponse.java
@@ -0,0 +1,34 @@
+package com.rest.entities;
+
+import java.util.Date;
+
+public class GetStudentErrorResponse {
+
+    private Date date ;
+    private  String errorClass ;
+    private String errorMessage ;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getErrorClass() {
+        return errorClass;
+    }
+
+    public void setErrorClass(String errorClass) {
+        this.errorClass = errorClass;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
